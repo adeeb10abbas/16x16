95bae5757d7a48921e59bfa8cf2515dcc4ce14e4
adeeb10abbas <adeebabbas@gmail.com>
2019-07-05 18:16:03 -0400

    Updated according to the latest v2 documentation

--- cli-controller.py
+++ cli-controller.py
@@ -1,31 +1,60 @@
 import click
 import requests
 import json
+
 @click.command()
-@click.option('--position', '-p', default = 1, help = "input the position(1-16)", type = int)
-@click.option('--switch', '-s', default = 1, help = "input the switch (1-16)", type = int)
-@click.option('--reset', '-r', is_flag = False ,default = False, help = "True sets everything to default")
-@click.option('--view', '-v', is_flag = False, default = False, help = "Use this to see the current status of the matrix")
-def main(switch, position, reset, view):
+@click.option('--position', '-p', default = 1, help = "input the position(1-16).", type = int)
+@click.option('--switch', '-s', default = 1, help = "input the switch (1-16).", type = int)
+@click.option('--reset', '-r', is_flag = False ,default = False, help = "True sets everything to default.")
+@click.option('--view', '-v', is_flag = False, default = False, help = "Use this to see the current status of the matrix.")
+@click.option('--attenuation', '-a', default = 0, help = "Set attenuation(0.0 - 92.0)", type = float)
+def main(switch, position, reset, view, attenuation):
     headers = {
     'Content-Type': 'application/json',
     }
     data = {}
+    adata={}
     data["position"] = int(position)
+    
+    adata["attenuation"] = float(attenuation)
     json_data = json.dumps(data)
+    json_adata = json.dumps(adata)
     
 
-    response = requests.post('http://10.248.102.11//v1/switch/'+str(switch), headers=headers, data=str(json_data))
-    click.echo(response.text)
-    
-
+    response = requests.post('http://10.248.102.11/v2/switches/'+str(switch), headers=headers, data=str(json_data))
+   
+    print(response.text)
     if reset:
-        response = requests.post("http://10.248.102.11//v1/switches/default-positions ",headers = headers, data= str(json_data))
-             
+        adata["attenuation"] = 0.0
+        data["position"] = int(1)
+        
+
+        response = requests.post("http://10.248.102.11//v2/defaults/switches ",headers = headers, data=str(json_data))
+        
+        json_adata = json.dumps(adata)
+        for i in range(1,17):
+               response = requests.post('http://10.248.102.11/v2/switches/'+str(i), headers=headers, data=str(json_data))
+               response = requests.post("http://10.248.102.11//v2/attenuators/"+str(i),headers = headers, data=json_adata)
+               response = requests.get("http://10.248.102.11//v2/attenuators/"+str(i))
+               print(response.text)
+        print("All switches and attenuation has been reset!")
+            
     if view:
-        print("Current Status")
+        print("<--------------Current Status------------>")
         response = requests.get('http://10.248.102.11//v1/switches/', headers= headers)
-        print(response.content)
+        a = str(response.text).split(',')
+        print(a)
+        print("<---------------------------------------->")
+    if int(attenuation)!=0:
+        response = requests.post("http://10.248.102.11//v2/attenuators/"+str(switch),headers = headers, data=json_adata)
+        #print(response.text)
+        print("Switch", switch,"at position", position, "has an attenuation of", attenuation) 
+        print()
+        for i in range(1,17):
+               response = requests.get("http://10.248.102.11//v2/attenuators/"+str(i))
+               print(response.text)
+
+
 if __name__ == "__main__":
     main()
     
