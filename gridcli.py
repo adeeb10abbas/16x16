002d1c854c6b9e4acd5325e092a7a4ae524224f4
adeeb10abbas <adeebabbas@gmail.com>
2019-07-10 13:29:56 -0400

    Updated requirements

--- gridcli.py
+++ gridcli.py
@@ -1,9 +1,83 @@
 # -*- coding: utf-8 -*-
-
+#!/usr/bin/env python3
 """ CLI for 16x16 matrix """
 
 import argparse
-from lxd_commands import *
+from gridcli import *
+import urllib3
+import requests
+import json
+headers = {
+    'Content-Type': 'application/json',
+    }
+def set_position(position, switch):
+    headers = {
+    'Content-Type': 'application/json',
+    }
+    data = {}
+    data["position"] = int(position)
+    json_data = json.dumps(data)
+    response = requests.post('http://10.248.102.11/v2/switches/'+str(switch), headers=headers, data=str(json_data))
+
+def reset_to_defaultA():
+    adata = {}
+    
+    adata["attenuation"] = 50.0
+    
+    json_adata = json.dumps(adata)
+    
+    #response = requests.post("http://10.248.102.11//v2/defaults/switches ",headers = headers, data=str(json_data))
+    
+    
+    for i in range(1,17):
+        #response = requests.post('http://10.248.102.11/v2/switches/'+str(i), headers=headers, data=str(json_data))
+        response = requests.post("http://10.248.102.11//v2/attenuators/"+str(i),headers = headers, data=json_adata)
+        #response = requests.get("http://10.248.102.11//v2/attenuators/"+str(i))
+        #print(response.text)
+    print("Attenuation(default value: 50.0) values has been reset!")
+def reset_to_defaultS():
+    data ={}
+    data["position"] = int(1)
+    json_data = json.dumps(data)
+
+    for i in range(1,17):
+        response = requests.post('http://10.248.102.11/v2/switches/'+str(i), headers=headers, data=str(json_data))
+        
+        #print(response.text)
+    print("The switches and the positions have been reset!")
+
+
+
+
+
+
+
+def view(view):
+    print("<-------------- Status------------>")
+    for i in range(1,17):
+        response = requests.get('http://10.248.102.11//v2/switches/'+str(i), headers= headers)
+        response2 = requests.get("http://10.248.102.11//v2/attenuators/"+str(i))
+        a = response.text.split(',')
+        ab = response2.text.split(',')
+        
+        print("switch"+a[0][5:], a[1][12:], ab[1][12:])
+    print("<---------------------------------------->")
+def attenuation(attenuation, switch, position):
+    headers = {
+    'Content-Type': 'application/json',
+    }
+    adata = {}
+    adata["attenuation"] = float(attenuation)
+    json_adata = json.dumps(adata)
+    response = requests.post("http://10.248.102.11//v2/attenuators/"+str(switch),headers = headers, data=json_adata)
+    #print(response.text)
+    print("Switch", switch,"at position", position, "has an attenuation of", attenuation) 
+    print()
+    # for i in range(1,17):
+    #     response = requests.get("http://10.248.102.11//v2/attenuators/"+str(i))
+    #     print(response.text.split(' '))
+
+
 
 def get_args():
     """ Get args from command line """
@@ -18,11 +92,16 @@ def get_args():
                         dest='switch',
                         default= int(1),
                         help='Set the switch(1-16)')
-    parser.add_argument( "-r",'--reset',
-                        dest='reset',
+    parser.add_argument( "-rA",'--resetA',
+                        dest='resetA',
                         default=False, 
                         action='store_true',
-                        help=' Reset all the switches, positions and attenuations')
+                        help=' Reset all the attenuations to 50.0')
+    parser.add_argument( "-rS",'--resetS',
+                        dest='resetS',
+                        default=False, 
+                        action='store_true',
+                        help=' Reset all the switches and positions')
     parser.add_argument('-v' ,'--view',
                         dest='view',
                         default=False,
@@ -47,33 +126,15 @@ def main():
         set_position(args.position, args.switch)
     if args.position:
         set_position(args.position, args.switch)
-    if args.reset:
-        reset_to_default()
+    if args.resetA:
+        reset_to_defaultA()
+    if args.resetS:
+        reset_to_defaultS()
     if float(args.attenuation)!=0:
         attenuation(args.attenuation, args.switch, args.position)
 
-    # elif args.gridnode:
-    #     if args.gridnode in grid_nodes:
-    #         # Do stuff
-    #         if args.start:
-    #             # Check if image source is provided
-    #             if args.image:
-    #                 print("\nStarting container on {}".format(args.gridnode))
-    #                 start_container(args.gridnode, args.image)
-    #             else:
-    #                 print("\nImage source not provided. Use -i or --image to"\
-    #                        "provide a source image from DWSL radio image server\n")
-    #         elif args.stop:
-    #             # Grid node is already provided, so run function
-    #             stop_delete_container(args.gridnode)
-    #         elif args.ipaddresscheck:
-    #             # Print container IP address on gridnode
-    #             print(ip_check_container(args.gridnode))
-    #         else:
-    #             # Neither start nor stop provided
-    #             print("\nOperation on grid node not provided. Use --start or"\
-    #                     "--stop to start or stop a container on a grid node\n")
-    #     else:
-    #         print("\n{} is not a valid grid node.\n".format(args.gridnode))
-    # else:
-    #     print("Grid node not provided. Use -gn or --gridnode")
+if __name__ == "__main__":
+    main()
+    
+
+
